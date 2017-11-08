282d4c1566f4054519d5291300b6aa5d14ed86ab
maddrum <maddrum9@gmail.com>
2017-11-08 12:07:18 +0200

    ver1

--- old_exam_problems/pool_pipes.py
+++ old_exam_problems/pool_pipes.py
@@ -3,17 +3,18 @@ p1=int(input())
 p2=int(input())
 h=float(input())
 h=round(h,2)
-p1Flow=p1*he
+p1Flow=p1*h
 p2Flow=p2*h
 totalFLow=p1Flow+p2Flow
-if v>=totalFLow:
-    x=int(totalFLow/v*100)
-    y=int(p1Flow/totalFLow*100)
-    z=int(p2Flow/totalFLow*100)
-    result="The pool is {}% full. Pipe 1: {}%. Pipe 2: {}%.".format(x,y,z)
-    print(result)
+if v>= totalFLow:
+    x = totalFLow/v*100//1
+    y = p1Flow/totalFLow*100//1
+    z = p2Flow/totalFLow*100//1
+    print("The pool is "+"{0:.0f}".format(x)+"% full. Pipe 1: "+"{0:.0f}".format(y)+"%. Pipe 2: "+"{0:.0f}".format(z)+"%.")
 else:
-    overfill=totalFLow-v
-    overfill="{0:.1f}".format(overfill)
-    result = "For {} hours the pool overflows with {} liters.".format(h,overfill)
-    print(result)
+    overfill = totalFLow-v
+    if h%1 != 0:
+        print("For {} hours the pool overflows with ".format(h) +"{0:.1f}".format(overfill)+" liters.")
+    else:
+        h=h//1
+        print("For "+"{0:.0f}".format(h)+" hours the pool overflows with " + "{0:.1f}".format(overfill) + " liters.")
--- old_exam_problems/zelenchuchi_zadacha.py
+++ old_exam_problems/zelenchuchi_zadacha.py
@@ -1,23 +1,7 @@
-vegetablePrice = float(input("Цена на кг зеленчуци /лв/:"))
-while vegetablePrice < 0.001 or vegetablePrice > 1000.000:
-    vegetablePrice = round(float(input("Моля, коректна цена на кг зеленчуци /лв/:")),3)
-
-
-fruitPrice = float(input("Цена на кг плодове /лв/ :"))
-while fruitPrice < 0.001 or fruitPrice > 1000.000:
-    fruitPrice = round(float(input("КОРЕКНТА! Цена на кг плодове /лв/ :")), 3)
-
-
-totalVegetables1 = float(input("Общо зеленчуци /кг/:"))
-while totalVegetables1 < 0.001 or totalVegetables1 > 1000.000:
-    totalVegetables1 = float(input("КОРЕКТНО Общо зеленчуци /кг/:"))
-totalVegetables = int(totalVegetables1)
-
-
-totalFruit1 = float(input("Общо плодове /кг/:"))
-while totalFruit1 < 0.001 or totalFruit1 > 1000.000:
-    totalFruit1 = float(input("КОРЕКТНО! Общо плодове /кг/:"))
-totalFruit = int(totalFruit1)
+vegetablePrice = float(input())
+fruitPrice = float(input())
+totalVegetables = float(input())
+totalFruit = float(input())
 
 
 totalRevLv=vegetablePrice*totalVegetables + fruitPrice*totalFruit
